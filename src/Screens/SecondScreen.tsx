fa6cd97cf81ca197e125ae8e2ae0326db82ee930
bog-Ardidi <georgip624@gmail.com>
2023-11-18 19:26:45 +0000

    Added working chart library

--- src/Screens/SecondScreen.tsx
+++ src/Screens/SecondScreen.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View } from "react-native";
 import {
   Layout,
@@ -8,9 +8,39 @@ import {
   useTheme,
 } from "react-native-rapi-ui";
 import { Ionicons } from "@expo/vector-icons";
+import {
+  VictoryChart,
+  VictoryZoomContainer,
+  VictoryScatter,
+} from "victory-native";
+// @ts-ignore
+import { range, random } from "lodash";
+
+const data = [
+  { quarter: 1, earnings: 13000 },
+  { quarter: 2, earnings: 16500 },
+  { quarter: 3, earnings: 14250 },
+  { quarter: 4, earnings: 19000 },
+];
+
+function getScatterData() {
+  return range(50).map((index: number) => {
+    return {
+      x: random(1, 50),
+      y: random(10, 90),
+      size: random(8) + 3,
+    };
+  });
+}
 
 export default function ({ navigation }: any) {
   const { isDarkmode, setTheme } = useTheme();
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    setData(getScatterData);
+  }, []);
+
   return (
     <Layout>
       <TopNav
@@ -45,8 +75,25 @@ export default function ({ navigation }: any) {
           justifyContent: "center",
         }}
       >
-        {/* This text using ubuntu font */}
-        <Text fontWeight="bold">This is the second screen</Text>
+        {/* <VictoryChart width={350} theme={VictoryTheme.material}>
+          <VictoryBar data={data} x="quarter" y="earnings" />
+        </VictoryChart> */}
+        <VictoryChart
+          domain={{ y: [0, 100] }}
+          containerComponent={
+            <VictoryZoomContainer zoomDomain={{ x: [5, 35], y: [0, 100] }} />
+          }
+        >
+          <VictoryScatter
+            data={data}
+            style={{
+              data: {
+                opacity: ({ datum }) => (datum.y % 5 === 0 ? 1 : 0.7),
+                fill: ({ datum }) => (datum.y % 5 === 0 ? "tomato" : "black"),
+              },
+            }}
+          />
+        </VictoryChart>
       </View>
     </Layout>
   );
